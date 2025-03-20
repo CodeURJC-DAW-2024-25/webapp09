884441ca6070a55c26618bd9f8354aa594de5999
Juan Antonio Alcalde Yuste <117301204+Jantoniio3@users.noreply.github.com>
2025-03-20 23:50:37 +0100

    crear reviewDTO

---
+++ practica1/src/main/java/es/grupo9/practica1/DTOs/ReviewDTO.java
@@ -0,0 +1,74 @@
+package es.grupo9.practica1.DTOs;
+
+import es.grupo9.practica1.entities.Review;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class ReviewDTO {
+
+    private Integer reviewId;
+    private Integer rating;
+    private String comment;
+    private Integer hotelCode;
+    private String userDni;
+
+    public ReviewDTO() {
+    }
+
+    public ReviewDTO(Integer reviewId, Integer rating, String comment, Integer hotelCode, String userDni) {
+        this.reviewId = reviewId;
+        this.rating = rating;
+        this.comment = comment;
+        this.hotelCode = hotelCode;
+        this.userDni = userDni;
+    }
+
+    public ReviewDTO(Review review) {
+        this.reviewId = review.getReviewId();
+        this.rating = review.getRating();
+        this.comment = review.getComment();
+        this.hotelCode = review.getHotel().getCode();
+        this.userDni = review.getUser().getDni();
+    }
+
+    public Integer getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(Integer reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Integer getHotelCode() {
+        return hotelCode;
+    }
+
+    public void setHotelCode(Integer hotelCode) {
+        this.hotelCode = hotelCode;
+    }
+
+    public String getUserDni() {
+        return userDni;
+    }
+
+    public void setUserDni(String userDni) {
+        this.userDni = userDni;
+    }
+}
