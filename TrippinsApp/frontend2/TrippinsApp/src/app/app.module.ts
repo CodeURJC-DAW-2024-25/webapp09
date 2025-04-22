1ea95bb9d59263417d23021d2850ad8b2905d461
Jadrados <s.adrados.2020@alumnos.urjc.es>
2025-04-22 16:11:15 +0200

    Componentes para cada una de nuestras paginas

--- TrippinsApp/frontend2/TrippinsApp/src/app/app.module.ts
+++ TrippinsApp/frontend2/TrippinsApp/src/app/app.module.ts
@@ -4,11 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { IndexComponent } from './components/index/index.component';
+import { RoomComponent } from './components/room/room.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ErrorComponent } from './components/error/error.component';
+import { LoginComponent } from './components/login/login.component';
+import { NewhotelComponent } from './components/newhotel/newhotel.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
+import { RoomDetailsComponent } from './components/room-details/room-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
+    RoomComponent,
+    AdminComponent,
+    ErrorComponent,
+    LoginComponent,
+    NewhotelComponent,
+    ProfileComponent,
+    RegisterComponent,
+    RoomDetailsComponent
   ],
   imports: [
     BrowserModule,
