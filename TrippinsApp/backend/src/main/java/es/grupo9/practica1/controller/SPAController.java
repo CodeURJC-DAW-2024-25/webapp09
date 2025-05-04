a0afb16ce05795c5dba2ec67a802126ee3ae0c69
Juan Antonio Alcalde Yuste <117301204+Jantoniio3@users.noreply.github.com>
2025-05-04 21:00:32 +0200

    spa controller

--- TrippinsApp/backend/src/main/java/es/grupo9/practica1/controller/SPAController.java
+++ TrippinsApp/backend/src/main/java/es/grupo9/practica1/controller/SPAController.java
@@ -1,4 +1,4 @@
-package es.grupo9.practica1.controller;
+package main.java.es.grupo9.practica1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
